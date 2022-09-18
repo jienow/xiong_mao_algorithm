8a791d1f9a8b989dceb46f0c33b3793b6ca9801f
jienow <2609482146@qq.com>
2022-09-18 22:55:07 +0800

    "测试更改"

--- test/cpp_02.cpp
+++ test/cpp_02.cpp
@@ -1,7 +1,26 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+// 	cout << "hello world" << endl;
+// 	cout << "hello wrold" << endl;
+// 	return 0;
+// }
+
+
+// 这个问题需要高版本c++,这里设置为20
 #include <iostream>
+#include <vector>
+#include <tuple>
 using namespace std;
-int main() {
-	cout << "hello world" << endl;
-	cout << "hello wrold" << endl;
+auto func(){
+	return tuple<int, int>{1, 2};
+}
+int main(){
+	tuple<int, int, int, int> t{1, 2, 3, 4};
+	auto [a, b, c, d] = t;
+	cout << a << b << c << d;
+	auto [e, f] = func();
+
 	return 0;
 }
+
