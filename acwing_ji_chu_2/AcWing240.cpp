62c1c213334cc8eca2bba6477bc8a4a3d6950370
jienow <2609482146@qq.com>
2022-10-02 00:27:28 +0800

    "2022/10/1"

---
+++ acwing_ji_chu_2/AcWing240.cpp
@@ -0,0 +1,59 @@
+/*
+
+*/
+
+#include <iostream>
+using namespace std;
+const int N = 50000 + 10;
+int p[N], d[N];
+int find(int x)
+{
+    if (p[x] != x)
+    {
+        int t = find(p[x]);
+        d[x] += d[p[x]];
+        p[x] = t;
+    }
+    return p[x];
+}
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        p[i] = i;
+    int res = 0;
+    while (m--)
+    {
+        int t, x, y;
+        scanf("%d%d%d", &t, &x, &y);
+        if (x > n || y > n) // 这个判断结束之后要continue
+            res++;
+        else
+        {
+            int px = find(x), py = find(y);
+            if (t == 1)
+            {
+                if (px == py && (d[x] - d[y]) % 3)
+                    res++;
+                else if (px != py)
+                {
+                    p[px] = py;
+                    d[px] = d[y] - d[x];
+                }
+            }
+            else
+            {
+                if (px == py && (d[x] - d[y] - 1) % 3)
+                    res++;
+                else if (px != py)
+                {
+                    p[px] = py;
+                    d[px] = d[y] + 1 - d[x];
+                }
+            }
+        }
+    }
+    printf("%d\n", res);
+    return 0;
+}
