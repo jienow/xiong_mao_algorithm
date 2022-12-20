c00e988eff2788f7e203dbe08a43a8f26eff01f8
jienow <2609482146@qq.com>
2022-12-20 16:03:07 +0800

    2022_12_20

--- acwing_ti_gao_1/AcWing321.cc
+++ acwing_ti_gao_1/AcWing321.cc
@@ -27,68 +27,63 @@ $1 < n < 15$
 
 #### 输入样例：
 
-    3
-    1 1 1 1 1 1 1 3
-    1 1 1 1 1 1 1 1
-    1 1 1 1 1 1 1 1
-    1 1 1 1 1 1 1 1
-    1 1 1 1 1 1 1 1
-    1 1 1 1 1 1 1 1
-    1 1 1 1 1 1 1 0
-    1 1 1 1 1 1 0 3
-    
+	3
+	1 1 1 1 1 1 1 3
+	1 1 1 1 1 1 1 1
+	1 1 1 1 1 1 1 1
+	1 1 1 1 1 1 1 1
+	1 1 1 1 1 1 1 1
+	1 1 1 1 1 1 1 1
+	1 1 1 1 1 1 1 0
+	1 1 1 1 1 1 0 3
+
 
 #### 输出样例：
 
-    1.633
+	1.633
 */
 
 
 #include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cmath>
 using namespace std;
-const int N = 18, M = 9, INF = 0x3f3f3f3f;
+const int N = 15, M = 9, INF = 1e9;
+double f[M][M][M][M][N];
 int s[M][M];
-double f[N][N][N][N][M];
 int n, m = 8;
 double X;
 double get_sum(int x1, int y1, int x2, int y2) {
-    return s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1];
+	return s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1];
 }
 double get(int x1, int y1, int x2, int y2) {
-    int sum = get_sum(x1, y1, x2, y2) - X;
-    return (double) sum * sum / n;
+	double sum = get_sum(x1, y1, x2, y2) - X;
+	return (double)sum * sum / n;
 }
 double dp(int x1, int y1, int x2, int y2, int k) {
-    double& v = f[x1][y1][x2][y2][k];
-    if (v >= 0) return v;
-    if (k == 1) return v = get(x1, y1, x2, y2);
+	double& v = f[x1][y1][x2][y2][k];
+	if (v >= 0)return v;
+	if (k == 1) return v = get(x1, y1, x2, y2);
 
-    v = INF;
-    for (int i = x1; i < x2; i++)
-    {
-        v = min(v, get(i + 1,y1,x2,y2) + dp(x1, y1, i, y2, k - 1));
-        v = min(v, get(x1, y1, i, y2) + dp(i + 1, y1, x2, y2, k - 1));
-    }
-    for (int i = y1; i < y2; i++)
-    {
-        v = min(v, get(x1, y1, x2, i) + dp(x1, i + 1, x2, y2, k - 1));
-        v = min(v, get(x1, i + 1, x2, y2) + dp(x1, y1, x2, i,k-1));
-    }
-    return v;
+	v = INF;
+	for (int i = x1; i < x2; i++) {
+		v = min(v, get(x1, y1, i, y2) + dp(i + 1, y1, x2, y2, k - 1));
+		v = min(v, get(i + 1, y1, x2, y2) + dp(x1, y1, i, y2, k - 1));
+	}
+	for (int j = y1; j < y2; j++) {
+		v = min(v, get(x1, y1, x2, j) + dp(x1, j + 1, x2, y2, k - 1));
+		v = min(v, get(x1, j + 1, x2, y2) + dp(x1, y1, x2, j, k - 1));
+	}
+	return v;
 }
 int main() {
-    cin >> n;
-    for (int i = 1; i <= 8; i++)
-        for (int j = 1; j <= 8; j++)
-        {
-            cin >> s[i][j];
-            s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
-        }
-    X = (double)s[m][m] / n;
-    memset(f, -1, sizeof f);
-    printf("%.3lf", sqrt(dp(1, 1, 8, 8, n)));
+	cin >> n;
+	for (int i = 1; i <= m; i++)
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> s[i][j];
+			s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
+		}
+	X = (double)s[m][m] / n;
+	memset(f, -1, sizeof f);
+	printf("%.3lf", sqrt(dp(1, 1, 8, 8, n)));
 	return 0;
 }
