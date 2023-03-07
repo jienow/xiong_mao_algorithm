02e7b1f38983d1189ca2d76cc11aea222ed94587
jienow <2609482146@qq.com>
2023-03-07 23:20:38 +0800

    2023_3_7

--- acwing_ji_chu_1/AcWing801/main_1.cc
+++ acwing_ji_chu_1/AcWing801/main_1.cc
@@ -1,18 +1,21 @@
 #include <iostream>
 using namespace std;
-const int N = 1e5 + 10;
-int f[N], g[N];
-int n, m;
+const int N = 100000 + 10;
+int arr[N];
+int ge_shu(int num) {
+	int res = 0;
+	while (num) {
+		res += num & 1;
+		num >>= 1;
+	}
+	return res;
+}
 int main() {
 	ios::sync_with_stdio(false);
-	cin >> m >> n;
-	for (int i = 1; i <= m; i++) cin >> g[i];
-	for (int j = 1; j <= n; j++) cin >> f[j];
-	int i,j;
-	for (i = 1, j = 1; i <= n; i++) {
-		if (j <= m && f[i] == g[j]) j++;
+	int n; cin >> n;
+	while (n--) {
+		int num; cin >> num;
+		cout << ge_shu(num) << " ";
 	}
-	if (j == m + 1) cout << "Yes" << endl;
-	else cout << "No" << endl;
 	return 0;
 }
---
+++ acwing_ji_chu_1/AcWing802/main_1.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int N = 3e5 + 10;
+typedef pair<int, int> PII;
+int a[N], s[N];
+vector<int> alls;
+vector<PII> add, query;
+int find(int num) {
+	int l = 0, r = alls.size() - 1;
+	while (l < r) {
+		int mid = l + r >> 1;
+		if (alls[mid] >= num) r = mid;
+		else l = mid + 1;
+	}
+	return l + 1;
+}
+int main() {
+	ios::sync_with_stdio(false);
+	int n, m; cin >> n >> m;
+	while (n--) {
+		int x, c; cin >> x >> c;
+		add.push_back({ x,c });
+		alls.push_back(x);
+	}
+	while (m--) {
+		int l, r; cin >> l >> r;
+		alls.push_back(l);
+		alls.push_back(r);
+		query.push_back({ l,r });
+	}
+	sort(alls.begin(), alls.end());
+	alls.erase(unique(alls.begin(), alls.end()), alls.end());
+
+	for (auto& item : add) {
+		int x = find(item.first);
+		a[x] += item.second;
+	}
+	for (int i = 1; i <= alls.size(); i++)
+		s[i] += s[i - 1] + a[i];
+
+	for (auto& item : query) {
+		int x = find(item.first), y = find(item.second);
+		cout << s[y] - s[x - 1] << endl;
+	}
+
+	return 0;
+}
