cdc4bcc65309ec05e9dfd96d28b827b61c240b79
jienow <2609482146@qq.com>
2023-03-15 23:01:50 +0800

    2023_3_15

---
+++ acwing_ji_chu_5/AcWing2/main_1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+const int N = 1010;
+int arr[N];
+int main() {
+	ios::sync_with_stdio(false);
+	int n, m; cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		int v, w; cin >> v >> w;
+		for (int j = m; j >= v; j--)
+			arr[j] = max(arr[j],arr[j - v] + w);
+	}
+	cout << arr[m] << endl;
+	return 0;
+}
